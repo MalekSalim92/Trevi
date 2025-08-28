b9ba884517d4ae8728ce0abd4740da71cab35e68
Malek Brachemi <brachemi.malek92@gmail.com>
2025-08-27 20:42:32 -0400

    some fields for profile permissions and profiles

--- force-app/main/default/lwc/flowRedirectingComponent/flowRedirectingComponent.js
+++ force-app/main/default/lwc/flowRedirectingComponent/flowRedirectingComponent.js
@@ -1,5 +1,23 @@
 import { LightningElement} from 'lwc';
+import getRedirectUrl from '@salesforce/apex/ShopifyPaymentService.getRedirectUrl';
 
 export default class FlowRedirectingComponent extends LightningElement {
+       
     
+    async connectedCallback() {
+        console.log('Redirecting main window...');
+        try {
+            const redirectUrl = await getRedirectUrl();
+            console.log('Retrieved redirect URL:', redirectUrl);
+            
+            if (redirectUrl) {
+                // Redirect immediately
+                window.location.href = redirectUrl;
+            } else {
+                console.log('No redirect URL found in custom metadata');
+            }
+        } catch (error) {
+            console.error('Error retrieving redirect URL:', error);
+        }
+    }
 }
